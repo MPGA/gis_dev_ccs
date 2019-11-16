0710eaca7d16512f28b901fd41fea66e8247c3c9
MPGA <monicagandino@gmail.com>
2019-11-16 21:52:43 +0100

    fix index and map.js

--- map.js
+++ map.js
@@ -1,30 +1,25 @@
-import View from './ol/View';
-import Map from './ol/Map';
-import TileLayer from './ol/layer/Tile';
-import OSM from './ol/source/OSM';
-/* New imports */
-import VectorLayer from './ol/layer/Vector';
-import VectorSource from './ol/source/Vector';
-import GeoJSON from './ol/format/GeoJSON';
 
+// initialize the map
+var map = L.map('map').setView([10.48801,-66.87919], 6);
+    
+// load a tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 18,
+    minZoom: 3
+  }).addTo(map);
 
-new Map({
-    target,
-    view: new View({
-        center: [0, 0],
-        zoom: 2,
-    }),
-    layers: [
-        new TileLayer({
-            source: new OSM(),
-        }),
-        // New VectorLayer with VectorSource and the countries.geojson file as source
-        new VectorLayer({
-            source: new VectorSource({
-                url: './data/venezuela.geojson',
-                format: new GeoJSON(),
-            })
-        })
-    ]
+  // load GeoJSON from an external file
+$.getJSON("./data/venezuela.geojson",function(data){
+// add GeoJSON layer to the map once the file is loaded
+L.geoJson(data).addTo(map);
+
+var geoJsonLayer = L.geoJson(data, {style: styleFunction})
+.addTo(map);
+function styleFunction(){
+return {color: "#e0e0e0"};
+}
+
 });
 
